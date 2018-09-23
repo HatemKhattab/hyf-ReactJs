18b98e088b19a07add03c03d1c1be7e2bdd6ff32
Hatem Khattab <hattemkhattab@gmail.com>
2018-09-23 23:52:22 +0200

    seperate App.js

---
+++ week2/todo-app/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class App extends React.Component {
+
+  render(){
+    const todos = [
+      {description: 'Get out of bed', date: 'Wed Sep 13 2017'},
+      {description: 'Brush teeth', date: 'Thu Sep 14 2017'},
+      {description: 'Eat breakfast', date: 'Fri Sep 15 2017'}
+    ];
+    return(
+      <div>
+        <h1>Todo App</h1>
+        <ul>
+          {todos.map(item => <Todo todo={item} key={item.date}  />)}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
