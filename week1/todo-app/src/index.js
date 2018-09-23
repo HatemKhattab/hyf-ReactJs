bdbc3a9bb5e17a32d92bdce2c196a5fe411eb1ba
Hatem Khattab <hattemkhattab@gmail.com>
2018-09-23 12:16:46 +0200

    correct the date

--- week1/todo-app/src/index.js
+++ week1/todo-app/src/index.js
@@ -7,7 +7,7 @@ class App extends React.Component {
 
   render(){
     const todos = [
-      {description: 'Get out of bed', date: 'Get out of bed'},
+      {description: 'Get out of bed', date: 'Wed Sep 13 2017'},
       {description: 'Brush teeth', date: 'Thu Sep 14 2017'},
       {description: 'Eat breakfast', date: 'Fri Sep 15 2017'}
     ];
